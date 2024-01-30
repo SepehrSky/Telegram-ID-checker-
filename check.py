88e02eccaaeccb3869a604b686b80737039a4af9
Sepehr Najafpoor <39538613+SepehrSky@users.noreply.github.com>
2024-01-30 22:06:21 +0330

    Update check.py

--- check.py
+++ check.py
@@ -116,4 +116,5 @@ async def main():
             print("1 or 2 ... Please!")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
