64ded735dd6d84bac4965418e38dff4243f1faa2
Sepehr Najafpoor <39538613+SepehrSky@users.noreply.github.com>
2024-01-30 17:18:29 +0330

    Update check.py

--- check.py
+++ check.py
@@ -1,10 +1,9 @@
-from telethon import TelegramClient, functions, errors
+from telethon import TelegramClient, sync, functions, errors
+from telegram.ext import CommandHandler, Updater
+from telegram import Update
 import configparser
 import time
 import os
-import logging
-
-logging.basicConfig(level=logging.INFO)
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -14,7 +13,7 @@ api_hash = config.get('default', 'api_hash')
 bot_token = config.get('default', 'bot_token')
 
 if api_id == 'UPDATE ME' or api_hash == 'UPDATE ME' or bot_token == 'UPDATE ME':
-    logging.error("Please read the config.ini and README.md")
+    print("Please read the config.ini and README.md")
     input()
     exit()
 else:
@@ -22,24 +21,27 @@ else:
     client = TelegramClient('Checker', api_id, api_hash)
     client.start()
 
+updater = Updater(bot_token, use_context=True)
+dispatcher = updater.dispatcher
+
 def user_lookup(account):
     try:
         result = client(functions.account.CheckUsernameRequest(username=account))
         log_word(account, "checked_words.txt")  # Log all checked usernames
 
         if result:
-            logging.info(f"The telegram {account} is available")
+            print("The telegram", account, "is available")
             log_word(account, "Available.txt")  # Log available usernames
         else:
-            logging.info(f"The telegram {account} is not available")
+            print("The telegram", account, "is not available")
     except errors.FloodWaitError as fW:
-        logging.warning(f"Hit the rate limit, waiting {fW.seconds} seconds")
+        print(f"Hit the rate limit, waiting {fW.seconds} seconds")
         time.sleep(fW.seconds)
         user_lookup(account)
     except errors.UsernameInvalidError as uI:
-        logging.error("Username is invalid")
+        print("Username is invalid")
     except errors.rpcbaseerrors.BadRequestError as bR:
-        logging.error(f"Error: {bR.message}")
+        print("Error:", bR.message)
 
 def log_word(word, filename):
     with open(filename, 'a') as file:
@@ -49,9 +51,6 @@ def remove_checked_words():
     checked_path = os.path.join("word_lists", "checked_words.txt")
     word_list_path = os.path.join("word_lists", config.get('default', 'wordList'))
 
-    logging.info("Checking path: {}".format(checked_path))
-    logging.info("Word list path: {}".format(word_list_path))
-
     if os.path.exists(checked_path) and os.path.exists(word_list_path):
         with open(checked_path, 'r', encoding='utf-8-sig') as checked_file:
             checked_words = checked_file.read().split('\n')
@@ -59,17 +58,12 @@ def remove_checked_words():
         with open(word_list_path, 'r', encoding='utf-8-sig') as word_list_file:
             word_list = word_list_file.read().split('\n')
 
-        logging.info("Checked Words: {}".format(checked_words))
-        logging.info("Original Word List: {}".format(word_list))
-
         remaining_words = [word for word in word_list if word not in checked_words]
-
-        logging.info("Remaining Words: {}".format(remaining_words))
 
         with open(word_list_path, 'w', encoding='utf-8-sig') as updated_file:
             updated_file.write('\n'.join(remaining_words))
 
-def get_words():
+def get_words(update: Update, context):
     delay = config.get('default', 'delay')
     path = os.path.join("word_lists", config.get('default', 'wordList'))
 
@@ -81,12 +75,13 @@ def get_words():
             user_lookup(name)
             time.sleep(int(delay))
 
-    logging.info("Removing checked words from the word list...")
+    print("Removing checked words from the word list...")
     remove_checked_words()
-    logging.info("All done")
+    print("All done")
+    context.bot.send_message(chat_id=update.message.chat_id, text="All done")
 
 def main():
-    logging.info('''
+    print('''
     ▄▄▄█████▓▓█████  ██▓    ▓█████   ▄████  ██▀███   ▄▄▄       ███▄ ▄███▓
     ▓  ██▒ ▓▒▓█   ▀ ▓██▒    ▓█   ▀  ██▒ ▀█▒▓██ ▒ ██▒▒████▄    ▓██▒▀█▀ ██▒
     ▒ ▓██░ ▒░▒███   ▒██░    ▒███   ▒██░▄▄▄░▓██ ░▄█ ▒▒██  ▀█▄  ▓██    ▓██░
@@ -109,11 +104,13 @@ def main():
                 name = input("Enter a username: ")
                 user_lookup(name)
             else:
-                logging.info("Getting usernames from word_lists...")
-                get_words()
+                print("Getting usernames from word_lists...")
+                get_words(None, None)
                 break
         else:
-            logging.warning("1 or 2 ... Please!")
+            print("1 or 2 ... Please!")
 
 if __name__ == "__main__":
     main()
+    updater.start_polling()
+    updater.idle()
