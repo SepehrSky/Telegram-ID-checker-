23623357cc2e9b8ab96217556dd38f14fbebae26
Sepehr Najafpoor <39538613+SepehrSky@users.noreply.github.com>
2024-01-30 22:01:08 +0330

    Update check.py

--- check.py
+++ check.py
@@ -1,15 +1,19 @@
-from telethon import TelegramClient, functions, errors
+from telethon import TelegramClient, sync, functions, errors
+from telegram.ext import CommandHandler, Updater
+from telegram import Update
 import configparser
 import time
 import os
+import asyncio
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 api_id = config.get('default', 'api_id')
 api_hash = config.get('default', 'api_hash')
+bot_token = config.get('default', 'bot_token')
 
-if api_id == 'UPDATE ME' or api_hash == 'UPDATE ME':
+if api_id == 'UPDATE ME' or api_hash == 'UPDATE ME' or bot_token == 'UPDATE ME':
     print("Please read the config.ini and README.md")
     input()
     exit()
@@ -18,9 +22,12 @@ else:
     client = TelegramClient('Checker', api_id, api_hash)
     client.start()
 
-def user_lookup(account):
+updater = Updater(bot_token, use_context=True)
+dispatcher = updater.dispatcher
+
+async def user_lookup(account):
     try:
-        result = client(functions.account.CheckUsernameRequest(username=account))
+        result = await client(functions.account.CheckUsernameRequest(username=account))
         log_word(account, "checked_words.txt")  # Log all checked usernames
 
         if result:
@@ -30,8 +37,8 @@ def user_lookup(account):
             print("The telegram", account, "is not available")
     except errors.FloodWaitError as fW:
         print(f"Hit the rate limit, waiting {fW.seconds} seconds")
-        time.sleep(fW.seconds)
-        user_lookup(account)
+        await asyncio.sleep(fW.seconds)
+        await user_lookup(account)
     except errors.UsernameInvalidError as uI:
         print("Username is invalid")
     except errors.rpcbaseerrors.BadRequestError as bR:
@@ -57,7 +64,7 @@ def remove_checked_words():
         with open(word_list_path, 'w', encoding='utf-8-sig') as updated_file:
             updated_file.write('\n'.join(remaining_words))
 
-def get_words():
+async def get_words():
     delay = config.get('default', 'delay')
     path = os.path.join("word_lists", config.get('default', 'wordList'))
 
@@ -66,17 +73,18 @@ def get_words():
             words = file.read().split('\n')
 
         for name in words:
-            user_lookup(name)
-            time.sleep(1 / 30)  # Sleep for 1/30 seconds between requests
+            await user_lookup(name)
+            await asyncio.sleep(1/30)  # Introduce the 1/30 second delay
 
     print("Removing checked words from the word list...")
     remove_checked_words()
     print("All done")
 
-def close():
+async def close():
     print("Closing the app.")
+    await client.disconnect()
 
-if __name__ == "__main__":
+async def main():
     print('''
     ▄▄▄█████▓▓█████  ██▓    ▓█████   ▄████  ██▀███   ▄▄▄       ███▄ ▄███▓
     ▓  ██▒ ▓▒▓█   ▀ ▓██▒    ▓█   ▀  ██▒ ▀█▒▓██ ▒ ██▒▒████▄    ▓██▒▀█▀ ██▒
@@ -98,11 +106,14 @@ if __name__ == "__main__":
         if option in set_options:
             if option == set_options[0]:
                 name = input("Enter a username: ")
-                user_lookup(name)
+                await user_lookup(name)
             else:
                 print("Getting usernames from word_lists...")
-                get_words()
-                close()  # Close the app
+                await get_words()
+                await close()  # Close the app
                 break
         else:
             print("1 or 2 ... Please!")
+
+if __name__ == "__main__":
+    asyncio.run(main())
