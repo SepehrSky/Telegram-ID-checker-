3765debc9b68634340d10942828fc4fc93e7e554
Sepehr Najafpoor <39538613+SepehrSky@users.noreply.github.com>
2024-01-31 03:38:57 +0330

    Update check.py

--- check.py
+++ check.py
@@ -22,7 +22,6 @@ async def user_lookup(account):
             print(f"The telegram {account} is not available")
     except errors.FloodWaitError as fW:
         print(f"Hit the rate limit, waiting {fW.seconds} seconds")
-        await asyncio.sleep(fW.seconds)
         await user_lookup(account)
     except errors.UsernameInvalidError as uI:
         print("Username is invalid")
@@ -36,7 +35,6 @@ async def user_lookup(account):
             print(f"The telegram {account} is available")
         elif "FLOOD_WAIT" in bR.message:
             print(f"Hit the rate limit, waiting {bR.seconds} seconds")
-            await asyncio.sleep(bR.seconds)
             await user_lookup(account)
         else:
             print("Unhandled error:", bR.message)
@@ -96,7 +94,6 @@ async def main():
                 await asyncio.sleep(fW.seconds)
                 print("Options after rate limit:")
                 await display_options()
-                continue  # Restart the loop to display the options
             except Exception as e:
                 print(f"Unhandled error: {e}")
                 await asyncio.sleep(5)
