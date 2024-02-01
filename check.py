63ab326a76283fa18235400f7b63715706596b2e
Sepehr Najafpoor <39538613+SepehrSky@users.noreply.github.com>
2024-02-01 17:31:49 +0330

    Update check.py

--- check.py
+++ check.py
@@ -1,8 +1,7 @@
-from telethon import TelegramClient, functions, errors
+from telethon.sync import TelegramClient, functions, errors
 import configparser
 import os
 import time
-import asyncio
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -25,19 +24,19 @@ class RateLimiter:
         self.limit_per_second = limit_per_second
         self.last_request_time = time.time()
 
-    async def delay_request(self):
+    def delay_request(self):
         current_time = time.time()
         time_difference = current_time - self.last_request_time
         if time_difference < 1 / self.limit_per_second:
             delay = 1 / self.limit_per_second - time_difference
-            await asyncio.sleep(delay)
+            time.sleep(delay)
         self.last_request_time = time.time()
 
 
-async def user_lookup(account, rate_limiter):
+def user_lookup(account, rate_limiter):
     try:
-        await rate_limiter.delay_request()
-        result = await client(functions.account.CheckUsernameRequest(username=account))
+        rate_limiter.delay_request()
+        result = client(functions.account.CheckUsernameRequest(username=account))
         if result == True:
             print("The telegram", account, "is available")
             file = open(output(), 'a')
@@ -47,14 +46,14 @@ async def user_lookup(account, rate_limiter):
             print("The telegram", account, "is not available")
     except errors.FloodWaitError as fW:
         print("Hit the rate limit, waiting", fW.seconds, "seconds")
-        await asyncio.sleep(fW.seconds)
+        time.sleep(fW.seconds)
     except errors.UsernameInvalidError as uI:
         print("Username is invalid")
     except errors.rpcbaseerrors.BadRequestError as bR:
         print("Error:", bR.message)
 
 
-async def get_words(rate_limiter):
+def get_words(rate_limiter):
     words = []
     delay = config.get('default', 'delay')
     path = os.path.join("word_lists", config.get('default', 'wordList'))
@@ -67,8 +66,8 @@ async def get_words(rate_limiter):
 
     for i in range(len(words)):
         name = words[i]
-        await user_lookup(name, rate_limiter)
-        await asyncio.sleep(int(delay))
+        user_lookup(name, rate_limiter)
+        time.sleep(int(delay))
     print("All done")
     input("Press enter to exit...")
 
@@ -77,7 +76,7 @@ def output():
     return config.get('default', 'outPut', fallback="AVAILABLE.txt")
 
 
-async def main():
+def main():
     print('''
     ▄▄▄█████▓▓█████  ██▓    ▓█████   ▄████  ██▀███   ▄▄▄       ███▄ ▄███▓
     ▓  ██▒ ▓▒▓█   ▀ ▓██▒    ▓█   ▀  ██▒ ▀█▒▓██ ▒ ██▒▒████▄    ▓██▒▀█▀ ██▒
@@ -94,21 +93,21 @@ async def main():
         bulk checking may result in false positives and longer wait times
             ''')
     print("1 = Enter username manually\n2 = Read a list of usernames from the word_lists folder")
-    options = ["1", "2"]
+    set = ["1", "2"]
     option = input("Select your option: ")
     rate_limiter = RateLimiter(0.5)  # Adjust the rate limit as needed
 
     while True:
-        if option in options:
-            if option == "1":
+        if str(option) in set:
+            if option == set[0]:
                 name = input("Enter a username: ")
-                await user_lookup(name, rate_limiter)
+                user_lookup(name, rate_limiter)
             else:
-                await get_words(rate_limiter)
+                get_words(rate_limiter)
                 break
         else:
             option = input("1 or 2 ... Please!: ")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
